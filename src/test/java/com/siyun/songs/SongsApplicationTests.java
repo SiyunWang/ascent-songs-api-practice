d4105e5c14e86a0c5ffa23575b445cb4eb669416
Siyun Wang <wangsiyun0518@gmail.com>
2021-05-17 13:11:35 -0700

    add getSongs with Params integartion tests

--- src/test/java/com/siyun/songs/SongsApplicationTests.java
+++ src/test/java/com/siyun/songs/SongsApplicationTests.java
@@ -11,9 +11,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SongsApplicationTests {
@@ -61,13 +68,45 @@ class SongsApplicationTests {
 	}
 
 	@Test
-	void getSongs_withParams_returnsNoContent_IfEmpty() {
+	void getSongs_noParams_returnsNoContent_IfEmpty() {
 		songsRepository.deleteAll();
 		ResponseEntity<SongsList> response = testRestTemplate.getForEntity("/api/songs", SongsList.class);
 		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
 		assertThat(response.getBody()).isNull();
 	}
 
+	@Test
+	void getSongs_withArtistAndAlbum_returnsSongsList_NotEmpty() throws Exception {
+		ResponseEntity<SongsList> response = testRestTemplate.getForEntity("/api/songs?artist=The Beatles&album=Abbey Road", SongsList.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(response.getBody()).isNotNull();
+		assertThat(response.getBody().isEmpty()).isFalse();
+		assertThat(response.getBody().getSongs().size()).isEqualTo(1);
+	}
 
+	@Test
+	void getSongs_withArtist_returnsSongsList_NotEmpty() throws Exception {
+		ResponseEntity<SongsList> response = testRestTemplate.getForEntity("/api/songs?artist=Weekend", SongsList.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(response.getBody()).isNotNull();
+		assertThat(response.getBody().isEmpty()).isFalse();
+		assertThat(response.getBody().getSongs().size()).isEqualTo(1);
+	}
+
+	@Test
+	void getSongs_withAlbum_returnsSongsList_NotEmpty() throws Exception {
+		ResponseEntity<SongsList> response = testRestTemplate.getForEntity("/api/songs?album=The Album", SongsList.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(response.getBody()).isNotNull();
+		assertThat(response.getBody().isEmpty()).isFalse();
+		assertThat(response.getBody().getSongs().size()).isEqualTo(2);
+	}
+
+	@Test
+	void getSongs_withParams_returnsNoContent_IfEmpty() throws Exception {
+		ResponseEntity<SongsList> response = testRestTemplate.getForEntity("/api/songs?artist=Weekend&album=Abbey Road", SongsList.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+		assertThat(response.getBody()).isNull();
+	}
 
 }
